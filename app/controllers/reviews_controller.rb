9e70849858000cfc110066332b1ecc4896fdf7cf
Ben McLaren <benmclaren@live.co.uk>
2020-01-31 14:20:02 +0000

    added show create and delete methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,12 +2,37 @@ class ReviewsController < ApplicationController
 
   def index
     # This is out list page for our reviews
-    @reviews = ["Nandos", "Five Guys", "Chipotle"]
+    @reviews = Review.all
   end
 
   def new
-    # This is the form for adding a new review 
+    # This is the form for adding a new review
     @review = Review.new
+  end
+
+  def create
+    # This takes information from the form and adds it to the database
+    @review = Review.new(params.require(:review).permit(:title, :body, :score))
+
+    # Save the review to the database
+    @review.save
+
+    # Redirect to homepage
+    redirect_to root_path
+  end
+
+  def show
+    # This is the individual show page
+    @review = Review.find(params[:id])
+  end
+
+  def destroy
+    # Find the individual review
+    @review = Review.find(params[:id])
+    # Destroy
+    @review.destroy
+    # Redirect to home homepage
+    redirect_to root_path
   end
 
 end

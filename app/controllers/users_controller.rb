723bc9a3bc41824cc8cd1fbe7c26020a00c8c4f9
Ben McLaren <benmclaren@live.co.uk>
2020-02-11 15:18:54 +0000

    added user model and sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all 
+  end
+
+  def new
+    # form for adding new user
+    @user = User.new
+  end
+
+  def create
+    # take parameters from the form and create a new form
+    # create a new user
+    # if its valid and it saves then got to the list users page
+    # if not see form with errors
+    @user = User.new(form_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render "new"
+    end
+  end
+
+  def form_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users
+  
   root "reviews#index"
 end
